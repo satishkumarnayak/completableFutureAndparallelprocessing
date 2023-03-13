86d6806f6e86d054609125c2b8389e4023d5e5f2
Satish Nayak <satish_nayak@epam.com>
2023-03-13 17:02:28 -0400

    first commit

---
+++ src/main/java/com/instanceofcake/competablefutureparallelprocessing/TestDrive.java
@@ -0,0 +1,11 @@
+package com.instanceofcake.competablefutureparallelprocessing;
+
+public class TestDrive {
+
+	public static void main(String[] args) {
+ 
+		System.out.println("Hello World!");
+
+	}
+
+}
